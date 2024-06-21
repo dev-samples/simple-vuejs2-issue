65a8c97060fb7905f45ae45595b53cbb5c51798c
your name <your email>
2024-06-21 22:15:54 +0200

    initial

---
+++ src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+    <nav>
+      <router-link to="/another">Another</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
---
+++ src/views/AnotherView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="another">
+    <h1>This is another page</h1>
+    <!--     <p>Coming from (referrer): {{ myvar }}</p> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AnotherView",
+  data() {
+    return {
+      myvar: "",
+    };
+  },
+  mounted() {
+    console.log("document.referrer", document.referrer);
+    this.myvar = document.referrer;
+
+    // Accessing the document content and referrer
+    this.documentContent = document.documentElement.outerHTML;
+    this.documentReferrer = document.referrer;
+
+    // Printing the document content and referrer to the console
+    //console.log("Document Content:", this.documentContent);
+    console.log(" - - Document Referrer:", this.documentReferrer);
+    //console.log("document:", document);
+    //console.log("document.referrer:", document.referrer);
+  },
+};
+</script>
